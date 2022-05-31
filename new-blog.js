0baabadf1ef046877f233caefbe88136b2039c3b
Pony Ma <pony.ma@student.com>
2022-05-31 10:51:14 +0800

    feat(new-blog): add hashnodeTitle

--- new-blog.js
+++ new-blog.js
@@ -1,14 +1,33 @@
 import process from 'node:process';
 import path from 'path';
 import fs from 'fs';
+
 function createNewBlog(blogName) {
+
+  const hashnodeTitle = `---
+title: # TODO
+subtitle: # TODO
+slug: ${blogName}
+tags: python3, python 
+
+/* You can find the list of tags here https://github.com/Hashnode/support/blob/main/misc/tags.json
+You need to upload your image to https://hashnode.com/uploader 
+and use the uploaded image URL as COVER_IMAGE_URL */ 
+
+cover: https://cdn.hashnode.com/res/hashnode/image/unsplash/PbzntH58GLQ/upload/v1653831013147/Tie1TT8RA.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress
+domain: pony.hashnode.dev
+---
+
+`;
+
   // get current file path in node
   fs.writeFileSync(
     path.join(process.cwd(), `${blogName}.md`),
-    `# ${blogName}`,
+    `${hashnodeTitle}`,
     'utf8'
   )
 
 }
 
-createNewBlog("s");
+const args = process.argv.slice(2)
+createNewBlog(args[0]);
